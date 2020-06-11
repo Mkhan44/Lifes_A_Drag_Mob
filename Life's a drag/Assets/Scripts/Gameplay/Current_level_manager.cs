6ba1731be87dcd23385a43c134a967af8be2fb3a
Mohamed R. Khan <khanantisora@hotmail.com>
2020-06-11 19:42:26 -0400

    Level Progression works
    Thinking of changing the item size (Shrinking it) on pickup as the items collision boxes are usually intersecting with multiple of the combo boxes.
    
    Also may re-factor the way i'm handling the levels figuring out which items are needed to make it easier on the backend side of things.

--- Life's a drag/Assets/Scripts/Gameplay/Current_level_manager.cs
+++ Life's a drag/Assets/Scripts/Gameplay/Current_level_manager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 public class Current_level_manager : MonoBehaviour
 {
     public Level_Manager theLev;
@@ -16,7 +17,7 @@ public class Current_level_manager : MonoBehaviour
     public Text timeForStarsText;
     public Text objectiveText;
     private TimeSpan currentTime;
-    public Text levelCompleteText;
+   
     //Use this bool for when we clear the level.
     private bool timerRunning;
     private float elapsedTime;
@@ -29,7 +30,9 @@ public class Current_level_manager : MonoBehaviour
 
     //Level completed variables.
     bool levelComplete = false;
-    
+    private string nextSceneName;
+    public Text levelCompleteText;
+    public GameObject nextLevelButton;
     public void Awake()
     {
         elapsedTime = 0f;
@@ -51,15 +54,13 @@ public class Current_level_manager : MonoBehaviour
 
     public void Start()
     {
-        /*
-        Debug.Log("The level name is: " + theLev.levelName);
-        Debug.Log("The level theme is: " + theLev.levelTheme);
-        Debug.Log("The difficulty is: " + theLev.levelDifficulty);
-        Debug.Log("The number of the level is: " + theLev.levelNum);
-        */
+        nextSceneName = theLev.levelDifficulty + "_" + theLev.levelTheme + "_" + theLev.levelNum.ToString();
+
+       // Debug.Log(nextSceneName);
 
 
         levelCompleteText.enabled = false;
+        nextLevelButton.SetActive(false);
         for (int i = 0; i < theLev.requiredItems.Count; i++)
         {
             Instantiate(theLev.requiredItems[i].item, new Vector3(theLev.requiredItems[i].xPos, theLev.requiredItems[i].yPos,0), Quaternion.identity);
@@ -68,15 +69,20 @@ public class Current_level_manager : MonoBehaviour
         //Calculate how many items are needed to complete the level.
         //The total number of combo items + the number of regular items that are not
         //materials for a combo item.
-        int numRegItems = 0;
+        int numMats = 0;
         for (int j = 0; j < theLev.comboItemsNeeded.Count; j++)
         {
-            if (!(theLev.requiredItems[j].item.gameObject.name == theLev.comboItemsNeeded[j].mat1 || theLev.requiredItems[j].item.gameObject.name == theLev.comboItemsNeeded[j].mat2))
+            for (int k = 0; k < theLev.requiredItems.Count; k++ )
             {
-                numRegItems += 1;
+                if ((theLev.requiredItems[k].item.gameObject.name == theLev.comboItemsNeeded[j].mat1 || theLev.requiredItems[k].item.gameObject.name == theLev.comboItemsNeeded[j].mat2))
+                {
+                    numMats += 1;
+                }
             }
+               
         }
-        itemsLeft = (theLev.comboItemsNeeded.Count + numRegItems);
+        Debug.Log(numMats);
+        itemsLeft = (theLev.comboItemsNeeded.Count + (theLev.requiredItems.Count - numMats));
         numItemsLeftText.text = "Items left: " + itemsLeft;
     }
     void Update()
@@ -194,7 +200,7 @@ public class Current_level_manager : MonoBehaviour
                 break;
             }
            
-            else
+            if(i == theLev.comboItemsNeeded.Count-1)
             {
                 //Item combo failed, try again!
                 Debug.Log("Combo failed!");
@@ -248,10 +254,61 @@ public class Current_level_manager : MonoBehaviour
         numItemsLeftText.text = "Items left: " + itemsLeft;
             if (itemsLeft == 0)
             {
-                Debug.Log("You found all the items!");
-                levelComplete = true;
-                levelCompleteText.enabled = true;
-                //Call level Complete function! (TO BE ADDED)
+                levelCompleted();
             }
     }
+
+    void levelCompleted()
+    {
+        Debug.Log("You found all the items!");
+        //Make the level up buttons visible. Make a pop up that shows your best time, etc.
+        levelComplete = true;
+        levelCompleteText.enabled = true;
+        if(DoesSceneExist(nextSceneName))
+        {
+           nextLevelButton.SetActive(true);
+        }
+
+        //Test code for seeing if level progression works.
+        nextLevelButton.SetActive(true);
+
+        //Test to ensure there is a level after this one. If not; don't let the 'next level' button appear.
+        //Have a retry button to restart the scene if the player wants to try again.
+        //Have a quit to menu button for the player to quit to the menu.
+        //If we implement it, have the player able to share their score to social media from this as well.
+        //Save the player's best time if they beat it. 
+        //Show animation of how many stars out of 3 the player received.
+    }
+
+   public void loadNextLevel()
+    {
+        string tempLoad = "testlevel2";
+
+        SceneManager.LoadScene(tempLoad);
+
+        //Use this code for the real game.
+       //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        //SceneManager.LoadScene(nextSceneName);
+    }
+
+   /// <summary>
+   /// Returns true if the scene 'name' exists and is in your Build settings, false otherwise
+   /// </summary>
+   public static bool DoesSceneExist(string name)
+   {
+       if (string.IsNullOrEmpty(name))
+           return false;
+
+       for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+       {
+           var scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+           var lastSlash = scenePath.LastIndexOf("/");
+           var sceneName = scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1);
+
+           if (string.Compare(name, sceneName, true) == 0)
+               return true;
+       }
+
+       return false;
+   }
 }
