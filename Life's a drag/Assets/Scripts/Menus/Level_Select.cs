68a57abd0e99d91190d9c7248cbeb453ed14c170
Mohamed R. Khan <khanantisora@hotmail.com>
2020-07-08 17:05:44 -0400

    Created basic level select
    Level selection now works for Easy Office levels. will make it more into a template like structure later on, but it's functional for this first version.

---
+++ Life's a drag/Assets/Scripts/Menus/Level_Select.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level_Select : MonoBehaviour
+{
+
+    public void loadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
