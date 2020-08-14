c952ffe7fa70a8fed714655026fbf027176924d6
Mohamed R. Khan <khanantisora@hotmail.com>
2020-08-14 18:20:26 -0400

    Initial step for menu dragging works
    Slide to unlock type thing is coming together. Working on it more soon.

--- Life's a drag/Assets/Scripts/Menus/Menu_Slider.cs
+++ Life's a drag/Assets/Scripts/Menus/Menu_Slider.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Menu_Slider : MonoBehaviour
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Menu_Slider : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+
+
+    public Vector3 initialPos;
+    public float yClampMin;
+    float yClampMax;
+    public float xClampMax;
+    public float xClampMin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPos = gameObject.transform.position;
+        yClampMin = gameObject.transform.position.y;
+        yClampMax = gameObject.transform.position.y;
     }
 
     // Update is called once per frame
@@ -15,4 +26,21 @@ public class Menu_Slider : MonoBehaviour
     {
         
     }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+       // throw new System.NotImplementedException();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector2 (Mathf.Clamp(Input.mousePosition.x, 80, 250), Mathf.Clamp(Input.mousePosition.y, yClampMin, yClampMax));
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = initialPos;
+    }
+
+
+ 
 }
