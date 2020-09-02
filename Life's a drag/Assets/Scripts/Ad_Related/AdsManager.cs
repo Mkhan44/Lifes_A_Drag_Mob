ae75833c5a193bbe8ef72129a08fd8d27384a135
Mohamed R. Khan <khanantisora@hotmail.com>
2020-09-02 00:54:15 -0400

    Video ads and banner ads started on
    Began work on implementing ads into the game. They work, now just need to make them appear appropriately for the player as well as ad no ads purchase option.

---
+++ Life's a drag/Assets/Scripts/Ad_Related/AdsManager.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using Hellmade.Sound;
+
+public class AdsManager : MonoBehaviour , IUnityAdsListener
+{
+    private string playStoreID = "3633238";
+    private string appStoreID = "3633239";
+
+    private string interstitialAd = "video";
+    private string rewardedVideoAd = "rewardedVideo";
+
+    public bool isTargetPlayStore;
+    public bool isTestAd;
+
+    string globalVolumeKey = "Global_Volume";
+    string MusicVolumeKey = "Music_Volume";
+    string SFXVolumeKey = "SFX_Volume";
+
+    float globalVolumePref;
+    float MusicVolumePref;
+    float SFXVolumePref;
+
+    public static AdsManager instance;
+    void Awake()
+    {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(this);
+
+    }
+    void Start()
+    {
+       
+        Advertisement.AddListener(this);
+        initializeAds();
+
+        globalVolumePref = PlayerPrefs.GetFloat(globalVolumeKey);
+        MusicVolumePref = PlayerPrefs.GetFloat(MusicVolumeKey);
+        SFXVolumePref = PlayerPrefs.GetFloat(SFXVolumeKey);
+    }
+
+    private void initializeAds()
+    {
+        if(isTargetPlayStore)
+        {
+            Advertisement.Initialize(playStoreID, isTestAd);
+            return;
+        }
+        else
+        {
+            Advertisement.Initialize(appStoreID, isTestAd);
+        }
+    }
+
+    public void playInterstitialAd()
+    {
+        if(!Advertisement.IsReady(interstitialAd))
+        {
+           
+            return;
+        }
+        else
+        {
+            Advertisement.Show(interstitialAd);
+        }
+    }
+
+    public void playRewardedVideoAd()
+    {
+        if (!Advertisement.IsReady(rewardedVideoAd))
+        {
+
+            return;
+        }
+        else
+        {
+            Advertisement.Show(rewardedVideoAd);
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+    //    throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+    //    throw new System.NotImplementedException();
+        switch(showResult)
+        {
+            case ShowResult.Failed:
+                {
+                    break;
+                }
+            case ShowResult.Skipped:
+                {
+                    break;
+                }
+            case ShowResult.Finished:
+                {
+                    //User watched rewarded video, give them something.
+                    if(placementId == rewardedVideoAd)
+                    {
+                        Debug.Log("Rewarded video!");
+                    }
+                    else if(placementId == interstitialAd)
+                    {
+                        Debug.Log("Interstitial video!");
+                    }
+                    break;
+                }
+        }
+
+        EazySoundManager.GlobalVolume = globalVolumePref;
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+     //   throw new System.NotImplementedException();
+        EazySoundManager.GlobalVolume = 0f;
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+    //    throw new System.NotImplementedException();
+    }
+
+}
